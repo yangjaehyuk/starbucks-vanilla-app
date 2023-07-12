4a0d8eff3905c31948e3085140b9910110f5210d
yangjaehyuk <gndksakxk10@naver.com>
2023-07-12 23:19:16 +0900

    Feat: Add badge

--- js/main.js
+++ js/main.js
@@ -15,3 +15,25 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl=document.querySelector('.badges');
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.screenY);
+  if(window.scrollY > 500){
+    // 배지 요소 숨김
+    // gsap.to(애니메이션 요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  }
+  else{
+    // 배지 요소 보임
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
+
+// _.throttle(함수, 시간)
