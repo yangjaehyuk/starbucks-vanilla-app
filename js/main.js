6b3995fe12de65f052a133c28db696afb319c6d1
yangjaehyuk <gndksakxk10@naver.com>
2023-07-13 14:53:52 +0900

    Feat: Add Youtube

--- js/main.js
+++ js/main.js
@@ -85,4 +85,25 @@ promotionToggleBtn.addEventListener('click', function () {
     // 보임 처리
     promotionEl.classList.remove('hide');
   }
-})
+});
+
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 '문자 데이터'를,
+  // `parseFloat()`을 통해 소수점을 가지는 '숫자 데이터'로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+};
+
+function floatingObject(selector, delay, size){
+  //gsap.to(요소, 시간, 옵션);
+  gsap.to(selector, random(1,5, 2.5), {
+    y: 20,
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easeInOut,
+    delay: random(0, delay)
+  });
+};
+
+floatingObject('.floating1', 1, 15)
+floatingObject('.floating2', .5, 15)
+floatingObject('.floating3', 1.5, 20)
